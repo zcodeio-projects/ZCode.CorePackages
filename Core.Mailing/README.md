d9384e7668cbc90fe7a542c6aabb6ecc7b954ab3
zahid2000 <zahidmamedov07@gmail.com>
2025-10-15 12:25:08 +0400

    Add mailing service

---
+++ Core.Mailing/README.md
@@ -0,0 +1,1 @@
+# ZCode.CorePackages
---
+++ Core.Mailing/Services/IMailService.cs
@@ -0,0 +1,10 @@
+﻿using Mail = ZCode.Core.Mailing.Models.Mail;
+
+namespace ZCode.Core.Mailing.Services;
+
+public interface IMailService
+{
+    void SendMail(Mail mail);
+    Task SendEmailAsync(Mail mail);
+}
+
